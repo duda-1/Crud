bebc35549a4d7265c0a0a2e78a4293b2bf59160a
Alunos <eduardacristinasilva2008@gmail.com>
2024-08-07 16:10:43 -0300

    Adicionar arquivos de projeto.

---
+++ Crud/Repositorio/TimeRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud.Repositorio
+{
+    public class TimeRepository
+    {
+        public SimuladorBD bd { get; set; }
+
+        public TimeRepository(SimuladorBD bdPreenchido)
+        {
+            bd = bdPreenchido;
+        }
+
+        public void Adicionar()
+        {
+
+        }
+
+        public void Remover()
+        {
+
+        }
+
+        public void Editar()
+        {
+
+        }
+
+        public void Listar()
+        {
+
+        }
+
+
+    }
+  
+}
---
+++ Crud/SimuladorBD.cs
@@ -0,0 +1,21 @@
+﻿using Crud.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud
+{
+    public class SimuladorBD
+    {
+        public List<Time> Times { get; set; }
+
+        public SimuladorBD()
+        {
+            Times = new List<Time>();
+        }
+
+
+    }
+}
