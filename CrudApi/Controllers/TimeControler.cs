c9f335a67b95a87024be3204c3525a7696d7c590
Alunos <eduardacristinasilva2008@gmail.com>
2024-08-13 17:22:09 -0300

    rfdfdfd

---
+++ CrudApi/Controllers/TimeControler.cs
@@ -0,0 +1,40 @@
+﻿using Crud;
+using Crud.Aplicação;
+using Crud.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeControler : ControllerBase
+    {
+        private TimeService _service;
+        private SimuladorBD bd;
+
+        public TimeControler(SimuladorBD bdSistema) 
+        {
+            bd = bdSistema;
+            _service = new TimeService(bd);
+        }
+
+        [HttpPost ("Adicionar_Time")]
+        public void AdicionmarTime(Time t)
+        {
+            _service.Adicionar(t);
+        }
+
+        [HttpGet ("Adicionar_Time")]
+        public void GetAdicionarTime()
+        {
+
+        }
+
+        [HttpGet("Listar_Time")]
+        public List<Time> GetListarTime()
+        {
+            return null;
+        }
+    }
+}
